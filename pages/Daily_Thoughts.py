218c2b409c2dec9e3d26873e706008f47f79a53a
Charanvir <charanvir123@gmail.com>
2023-01-20 22:46:02 -0500

    modified match case to if elif block because of streamlit cloud error

--- pages/Daily_Thoughts.py
+++ pages/Daily_Thoughts.py
@@ -7,25 +7,24 @@ thoughts = st.text_input("What are your thoughts on how today went")
 
 if thoughts:
     while True:
-        match date:
-            case "Monday":
-                filepath = "diary/1.Monday.txt"
-            case "Tuesday":
-                filepath = "diary/2.Tuesday.txt"
-            case "Wednesday":
-                filepath = "diary/3.Wednesday.txt"
-            case "Thursday":
-                filepath = "diary/4.Thursday.txt"
-            case "Friday":
-                filepath = "diary/5.Friday.txt"
-            case "Saturday":
-                filepath = "diary/6.Saturday.txt"
-            case "Sunday":
-                filepath = "diary/7.Sunday.txt"
-            case _:
-                print("Please select one of the dates")
+        if date == "Monday":
+            filepath = "diary/1.Monday.txt"
+        elif date == "Tuesday":
+            filepath = "diary/2.Tuesday.txt"
+        elif date == "Wednesday":
+            filepath = "diary/3.Wednesday.txt"
+        elif date == "Thursday":
+            filepath = "diary/4.Thursday.txt"
+        elif date == "Friday":
+            filepath = "diary/5.Friday.txt"
+        elif date == "Saturday":
+            filepath = "diary/6.Saturday.txt"
+        elif date == "Sunday":
+            filepath = "diary/7.Sunday.txt"
+        else:
+            print("Please select one of the dates")
         with open(filepath, "w") as file:
             file.write(thoughts)
-        break
+            break
 elif thoughts == "":
     st.text("Please enter a thought")
